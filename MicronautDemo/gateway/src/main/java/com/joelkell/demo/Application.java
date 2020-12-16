06d320a44d6beddc5fda1997dc152960fcb01a17
joelkell <joelkell75@gmail.com>
2020-12-16 21:34:55 +0000

    SwaggerUI update

--- MicronautDemo/gateway/src/main/java/com/joelkell/demo/Application.java
+++ MicronautDemo/gateway/src/main/java/com/joelkell/demo/Application.java
@@ -1,7 +1,20 @@
 package com.joelkell.demo;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Micronaut API",
+                version = "0.0",
+                description = "API for an Eccomerce Application",
+                license = @License(name = "Apache 2.0", url = "https://foo.bar"),
+                contact = @Contact(url = "https://joelkell.com", name = "Joel", email = "info@joelkell.com")
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
